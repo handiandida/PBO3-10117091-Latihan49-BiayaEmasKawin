49b742fc179b6bd5d7decd1c5682337d402f0006
Dida Handian <handiandida@gmail.com>
2018-10-23 03:26:48 +0700

    BiayaEmasKawin

---
+++ src/pbo3/pkg10117091/latihan49/biayaemaskawin/Emas.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan49.biayaemaskawin;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Emas {
+    private double totalBerat;
+    private int harga;
+
+    public double getTotalBerat() {
+        return totalBerat;
+    }
+
+    public void setTotalBerat(double totalBerat) {
+        this.totalBerat = totalBerat;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+    
+    public double perhitungan(){
+        return harga*totalBerat;
+    }
+    
+}
